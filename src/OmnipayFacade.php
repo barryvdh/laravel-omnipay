55c0d9c8ca0717bd3b09d8e1423aeeadbd1233ba
Gaël Reyrol <me@gaelreyrol.com>
2019-09-15 14:21:43 +0200

    chore: add phpdoc on facade

--- src/OmnipayFacade.php
+++ src/OmnipayFacade.php
@@ -4,6 +4,22 @@ namespace DansMaCulotte\Omnipay;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static OmnipayFacade setDefaultGateway(string $string)
+ * @method static OmnipayFacade withGateway(string $string)
+ * @method static \Omnipay\Common\Message\AbstractRequest authorize(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest completeAuthorize(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest capture(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest purchase(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest completePurchase(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest refund(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest void(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest createCard(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest updateCard(array $parameters)
+ * @method static \Omnipay\Common\Message\AbstractRequest deleteCard(array $parameters)
+ *
+ * @see OmnipayGatewayManager
+ */
 class OmnipayFacade extends Facade
 {
     /**
